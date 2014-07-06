74cdafbb8e936ce96df53837452c1ebd6b542ec4
Pavlenov Semen <me@pavlenov.ru>
2014-07-06 13:22:19 +0400

    + Cat

---
+++ src/scala/ru/pavlenov/scala/homework/rosalind/combinatorics/Cat.scala
@@ -0,0 +1,27 @@
+package ru.pavlenov.scala.homework.rosalind.combinatorics
+
+/**
+ * ⓭ + 52
+ * Какой сам? by Pavlenov Semen 05.07.14.
+ * Catalan Numbers and RNA Secondary Structures
+ * http://rosalind.info/problems/cat/
+ *
+ * Given:
+ * An RNA string s having the same number of occurrences of 'A' as 'U' and the same number of occurrences of 'C' as 'G'. The length of the string is at most 300 bp.
+ *
+ * Return:
+ * The total number of noncrossing perfect matchings of basepair edges in the bonding graph of s, modulo 1,000,000.
+ */
+
+object Cat {
+
+  def start() {
+
+    println("Start Cat")
+    println("Start: Catalan Numbers and RNA Secondary Structures = [http://rosalind.info/problems/cat/]")
+    println("==========================")
+
+
+  }
+
+}
---
+++ src/scala/ru/pavlenov/scala/utils/Comb.scala
@@ -0,0 +1,14 @@
+package ru.pavlenov.scala.utils
+
+/**
+ * ⓭ + 44
+ * Какой сам? by Pavlenov Semen 05.07.14.
+ * ${TITLE}
+ * ${URL}
+ *
+ * ${GIVEN}
+ * ${RETURN}
+ */
+object Comb {
+
+}
