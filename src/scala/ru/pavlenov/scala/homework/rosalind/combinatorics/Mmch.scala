85dcaf24202280801d34861ea3381cc1b2132e03
Pavlenov Semen <me@pavlenov.ru>
2014-07-06 14:35:55 +0400

    + Mmch

---
+++ src/scala/ru/pavlenov/scala/homework/rosalind/combinatorics/Mmch.scala
@@ -0,0 +1,27 @@
+package ru.pavlenov.scala.homework.rosalind.combinatorics
+
+/**
+ * ⓭ + 28
+ * Какой сам? by Pavlenov Semen 06.07.14.
+ * Maximum Matchings and RNA Secondary Structures
+ * http://rosalind.info/problems/mmch/
+ *
+ * Given:
+ * An RNA string s of length at most 100.
+ *
+ * Return:
+ * The total possible number of maximum matchings of basepair edges in the bonding graph of s.
+ */
+
+object Mmch {
+
+  def start() {
+
+    println("Maximum Matchings and RNA Secondary Structures")
+    println("from http://rosalind.info/problems/mmch/")
+    println("==========================")
+
+
+  }
+
+}
