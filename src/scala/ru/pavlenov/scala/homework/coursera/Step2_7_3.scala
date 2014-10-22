d48681e9070c7d4cd802d76ccf5183b33c128093
Pavlenov Semen <me@pavlenov.ru>
2014-10-22 21:50:47 +0400

    ===

---
+++ src/scala/ru/pavlenov/scala/homework/coursera/Step2_7_3.scala
@@ -0,0 +1,30 @@
+package ru.pavlenov.scala.homework.coursera
+
+import ru.pavlenov.scala.utils.File
+
+/**
+ * ⓭ + 58
+ * Какой сам? by Pavlenov Semen 21.10.14.
+ * Cyclopeptide Scoring Problem: Compute the score of a cyclic peptide against a spectrum.
+ * https://stepic.org/lesson/Mass-Spectrometry-Meets-Golf-102/step/3?course=Bioinformatics-Algorithms&unit=423
+ *
+ * Given:
+ * An amino acid string Peptide and a collection of integers Spectrum. 
+ *
+ * Return:
+ * The score of Peptide against Spectrum, Score(Peptide, Spectrum).
+ */
+
+object Step2_7_3 {
+
+  def start() {
+
+    println("Cyclopeptide Scoring Problem: Compute the score of a cyclic peptide against a spectrum.")
+    println("from https://stepic.org/lesson/Mass-Spectrometry-Meets-Golf-102/step/3?course=Bioinformatics-Algorithms&unit=423")
+    println("==========================")
+
+    val data = File.fromData(this)
+
+  }
+
+}
---
+++ src/scala/ru/pavlenov/scala/libs/peptide/Peptide.scala
@@ -0,0 +1,9 @@
+package ru.pavlenov.scala.libs.peptide
+
+/**
+ * ⓭ + 16
+ * Какой сам? by Pavlenov Semen 21.10.14.
+ */
+class Peptide {
+
+}
